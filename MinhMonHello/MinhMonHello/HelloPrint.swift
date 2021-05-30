ff29feb9940b929ff1b79ccdca076dfb3ed2a42d
Minh Mon <minhmon@192.168.2.101>
2021-05-30 17:37:30 +0700

    first commit

---
+++ MinhMonHello/MinhMonHello/HelloPrint.swift
@@ -0,0 +1,14 @@
+//
+//  HelloPrint.swift
+//  MinhMonHello
+//
+//  Created by Minh Mon on 30/05/2021.
+//
+
+import Foundation
+
+public class HelloPrint {
+    public static func hello() {
+        print("Hello World by Minh Mon")
+    }
+}
---
+++ MinhMonHello/MinhMonHello/MinhMonHello.h
@@ -0,0 +1,18 @@
+//
+//  MinhMonHello.h
+//  MinhMonHello
+//
+//  Created by Minh Mon on 30/05/2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MinhMonHello.
+FOUNDATION_EXPORT double MinhMonHelloVersionNumber;
+
+//! Project version string for MinhMonHello.
+FOUNDATION_EXPORT const unsigned char MinhMonHelloVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MinhMonHello/PublicHeader.h>
+
+
